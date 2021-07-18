e9f7f01c86559710b27ccdfd3320dee4a1788445
Olivier Jauze <ojauze@gmail.com>
2021-07-18 22:45:17 +0200

    cleanup local build + proposal for technology + minor fixes in some adoc

--- build-landing.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-WORKDIR=$(pwd)
-
-echo "Render Landing Page HTML content with Jekyll in 'dist' directory from 'landing' directory"
-docker run --rm --volume="$WORKDIR:/srv/jekyll" -it jekyll/jekyll:4.2.0 jekyll build --source landing --destination dist
--- build.sh
+++ build.sh
@@ -2,5 +2,17 @@
 
 WORKDIR=$(pwd)
 
-source build-docs.sh
-source build-landing.sh
+  case $1 in
+	build)
+		echo "Render web site HTML content including adoc with Jekyll in 'dist' directory from 'landing' directory"
+        docker run --rm --volume="$WORKDIR:/srv/jekyll" -it jekyll/jekyll:4.2.0 jekyll build --source landing --destination dist 
+		;;
+	serve)
+		echo "Render web site (HTML content including adoc with Jekyll in 'dist' directory from 'landing' directory) & serve it at 0.0.0.0:4000"
+		docker run --rm --volume="$WORKDIR:/srv/jekyll" --publish 4000:4000 -it jekyll/jekyll:4.2.0  jekyll serve --source landing --destination dist 
+		;;
+	*)
+		echo "Sorry, I don't understand"
+		;;
+  esac
+
