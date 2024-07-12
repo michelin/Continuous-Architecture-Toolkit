7c8795f37518a3cc190db03c14229b8f6b0be501
Thierry Fraudet <34861241+tfraudet@users.noreply.github.com>
2024-07-12 17:01:06 +0200

    Remove Michelin logo from generic architecture runway poster

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ WORKDIR=$(pwd)
 
 echo "Serve the 'dist' directory at http://0.0.0.0:8000"
 cd $WORKDIR/dist
-python -m SimpleHTTPServer
+# python -m SimpleHTTPServer
+python3 -m http.server 8000
 cd $WORKDIR
